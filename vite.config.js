edecd7d24caadc8bbff309ba84768a49d4deba0a
NAKAO-NONOKO <n-nakao@kakusin-g.co.jp>
2025-01-17 17:16:19 +0900

    初回コミット

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import autoprefixer from "autoprefixer";
+
+export default defineConfig({
+  root: 'src',
+  server: {
+    port: 3000,
+    open: './index.html'
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+  base: './',
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: [
+        'src/index.html',
+      ],
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          let filename = assetInfo.originalFileName;
+
+          if (filename != null) {
+            filename = filename.split('/');
+            filename.pop();
+          }
+            
+          if (/\.( gif|jpeg|jpg|png|svg|webp )$/.test(assetInfo.name)) {
+            return filename != null ? filename.join('/') + '/[name].[ext]' : 'assets/img/[name].[ext]' 
+          }
+          if (/\.css$/.test(assetInfo.name)) {
+            return 'assets/css/[name].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        }
+      }
+    },
+  },
+  envDir: './env'
+})
